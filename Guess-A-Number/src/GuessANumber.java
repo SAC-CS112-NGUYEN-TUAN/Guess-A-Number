24745ef703ec8f27bfb9d7605f793b916ff43f0c
SAC-CS112-NGUYEN-TUAN <tmacnguyen0525@gmail.com>
2015-09-30 20:00:04 -0700

    Initial program of GUESS A NUMBER

---
+++ Guess-A-Number/src/GuessANumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class GuessANumber {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int computerNum;
+		computerNum = 0 + (int) (Math.random()*10);
+		
+		System.out.println("Pls, input guess number:");
+		int guess = input.nextInt();
+		
+	if (guess == computerNum){
+		System.out.println("BINGO !!!");
+	}
+		else if (guess < computerNum) {
+			System.out.println("Your guess is too small");
+			
+		}
+		else if (guess > computerNum) {
+			System.out.println("Your guess it too big");
+		}
+	
+	System.out.println("Random number is :"+ computerNum);
+	}
+	
+
+}
